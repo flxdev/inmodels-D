a1a183fc55c0f807b8c91e9e7588a6b388ac18fa
Malash Dzmitrij <d.malash@flex.by>
2018-03-13 15:59:30 +0300

    final without captcha

--- src/js/lib/inputFocus.js
+++ src/js/lib/inputFocus.js
@@ -1,5 +1,5 @@
 import validator from 'jquery-form-validator/form-validator/jquery.form-validator.min.js';
-import 'jquery-form-validator/src/modules/security.js';
+// import 'jquery-form-validator/src/modules/security.js';
 import 'jquery-form-validator/src/modules/file.js';
 import autosize from 'autosize';
 
@@ -14,10 +14,12 @@ export default function setInputFocus() {
         form: form_this,
         validateOnBlur : true,
         validateHiddenInputs : true,
-        modules: 'security, file',
-        reCaptchaSiteKey: '6Ldb6EcUAAAAAGrmkKj7Q0ZEGXI0pzw-yvOXMhTE',
-        reCaptchaSize: 'normal',
-        reCaptchaTheme: 'light',
+        scrollToTopOnError : false,
+        modules: 'file',
+        // modules: 'security, file',
+        // reCaptchaSiteKey: '6Ldb6EcUAAAAAGrmkKj7Q0ZEGXI0pzw-yvOXMhTE',
+        // reCaptchaSize: 'normal',
+        // reCaptchaTheme: 'light',
       });
     });
   }
